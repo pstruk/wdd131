10bda224b4599c9c461711d4b3f89b98a0d6afd0
pstruk <str17021@byui.edu>
2024-07-07 01:05:38 -0400

    W11-Site Plan and wireframes

---
+++ paws_n_whiskers/paws.mjs
@@ -0,0 +1,112 @@
+const animals = [
+	{
+		name: 'Axel',
+		category: 'dogs',
+		breed: 'Labrador Retriever',
+		sex: 'Male',
+        weight: '15 pounds',
+		age: '2 years',
+		personality: ['Smart', 'Energetic', 'Runs'],
+		color: 'Black',
+		image: './images/axel_dog.jpg',
+		id: '248366'
+	},
+	{
+		name: 'Cooper',
+		category: 'cats',
+		breed: 'Domestic Short Hair',
+		sex: 'Male',
+        weight: '2.6 pounds',
+		age: '3 years',
+		personality: ['Sweet', 'Social', 'Playful'],
+		color: 'Orange Tabby',
+		image: './images/cooper_cat.jpg',
+		id: '249600'
+	},
+	{
+		name: 'Jericho',
+		category: 'dogs',
+		breed: 'Rottweiler',
+		sex: 'Male',
+        weight: '18 pounds',
+		age: '1 year',
+		personality: ['Friendly', 'Loyal', 'Energetic'],
+		color: 'Black',
+		image: './images/jericho_dog.jpg',
+		id: '250045'
+	},
+	{
+		name: 'Kylo',
+		category: 'cats',
+		breed: 'Domestic Short Hair',
+		sex: 'Male',
+        weight: '3 pounds',
+		age: '6 years',
+		personality: ['Smart', 'Playful', 'Independent'],
+		color: 'Grey Tabby',
+		image: './images/kylo_cat.jpg',
+		id: '248320'
+	},
+	{
+		name: 'Mustard',
+		category: 'rabbits',
+		breed: 'Dwarf',
+		sex: 'Male',
+        weight: '1 pound',
+		age: '3 years',
+		personality: ['Playful', 'Loves to Exercise'],
+		color: 'White',
+		image: './images/mustard_rabbit.jpg',
+		id: '231345'
+	},
+	{
+		name: 'Sapporo',
+		category: 'rabbits',
+		breed: 'Holland Lop',
+		sex: 'Female',
+        weight: '1.3 pounds',
+		age: '1 year',
+		personality: ['Friendly', 'Funny', 'Playful'],
+		color: 'Brown',
+		image: './images/sapporo_rabbit.jpg',
+		id: '247877'
+	},
+	{
+		name: 'Tiny',
+		category: 'cats',
+		breed: 'Domestic Short Hair',
+		sex: 'Male',
+        weight: '2.9 pounds',
+		age: '10 months',
+		personality: ['Sweet', 'Friendly', 'Cuddly'],
+		color: 'Black',
+		image: './images/tiny_the_cat.jpg',
+		id: '249992'
+	},
+    {
+        name: 'Toffee',
+		category: 'rabbits',
+		breed: 'French Lop',
+		sex: 'Male',
+        weight: '1 pound',
+		age: '2 years',
+		personality: ['Playful', 'Loves Grass'],
+		color: 'Brown',
+		image: './images/toffee_rabbit.jpg',
+		id: '249944'
+    },
+	{
+		name: 'Wesley',
+		category: 'dogs',
+		breed: 'Great Pyrenees',
+		sex: 'Male',
+        weight: '15.6 pounds',
+		age: '1 year',
+		personality: ['Gentle', 'Mellow', 'Shy'],
+		color: 'White',
+		image: './images/wesley_dog.jpg',
+		id: '249930'
+	}
+]
+
+export default animals
